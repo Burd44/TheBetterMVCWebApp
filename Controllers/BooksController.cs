bbbed5787cc6f47da7192cc2ac26450b556aa02b
Manolache Mihai <mihaimanolache03@gmail.com>
2022-03-17 21:22:05 +0200

    Added: bootswatch Theme (Slate), bootstrap 5 latest version support, added new navbar, added buttons to ad rows in db, added view to create row and modified it's controller by adding antiforgery validation and server side validation as well as error messages

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Data;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public BooksController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Books> books = _db.Books.ToList();
+            return View(books);
+        }
+        public IActionResult Create()
+        { 
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Books obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Books.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+    }
+}
